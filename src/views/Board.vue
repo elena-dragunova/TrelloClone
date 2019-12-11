518abf5c3ccbf1f33829c70d29001921f260dba9
elena-dragunova <elenak-8647@gmail.com>
2019-12-11 11:14:12 +0500

    Add adding and editing task's name and description.

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,24 +1,37 @@
 <template>
   <div class="board">
     <div class="flex flex-row items-start">
-      <div class="column" v-for="(column, $columnIndex) in board.columns" :key="$columnIndex">
+      <div class="column"
+           v-for="(column, $columnIndex) in board.columns"
+           :key="$columnIndex">
         <div class="flex items-center mb-2 font-bold">
            {{ column.name }}
         </div>
         <div class="list-reset">
-          <div class="task" v-for="(task, $taskIndex) in column.tasks" :key="$taskIndex" @click="goToTask(task)">
+          <div class="task"
+               v-for="(task, $taskIndex) in column.tasks"
+               :key="$taskIndex"
+               @click="goToTask(task)">
             <span class="w-full flex-no-shrink font-bold">
               {{ task.name }}
             </span>
-            <p v-if="task.description" class="w-full flex-no-shrink mt-1 text-sm">
+            <p v-if="task.description"
+               class="w-full flex-no-shrink mt-1 text-sm">
               {{ task.description }}
             </p>
           </div>
+
+          <input type="text"
+                 class="block p-2 w-full bg-transparent"
+                 placeholder="+ Enter new task" @keyup.enter="createTask($event, column.tasks)">
+
         </div>
       </div>
     </div>
 
-    <div v-if="isTaskOpen" class="task-bg" @click.self="close">
+    <div v-if="isTaskOpen"
+         class="task-bg"
+         @click.self="close">
       <router-view />
     </div>
 
@@ -40,6 +53,13 @@ export default {
     },
     close () {
       this.$router.push({ name: 'board' })
+    },
+    createTask (e, tasks) {
+      this.$store.commit('CREATE_TASK', {
+        tasks,
+        name: e.target.value
+      })
+      e.target.value = ''
     }
   }
 }
